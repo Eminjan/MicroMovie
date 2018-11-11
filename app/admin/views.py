c7767ef646ab456b4f57e95dcee48c12b7cbe893
Eminjan <eminjan123@aliyun.com>
2018-11-11 23:35:48 +0800

    添加后台1

--- app/admin/views.py
+++ app/admin/views.py
@@ -7,7 +7,7 @@ from flask import render_template,redirect,url_for
 
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'>Eminjan</h1>"
+    return render_template("admin/index.html")
 
 @admin.route("/login")
 def login():
