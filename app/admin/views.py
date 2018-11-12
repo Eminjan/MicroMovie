30cd1e3d95ff883fb5012056b0fb3a70c173452b
Eminjan <eminjan123@aliyun.com>
2018-11-12 10:43:36 +0800

    添加标签，电影列表，修改密码

--- app/admin/views.py
+++ app/admin/views.py
@@ -9,10 +9,35 @@ from flask import render_template,redirect,url_for
 def index():
     return render_template("admin/index.html")
 
-@admin.route("/login")
+@admin.route("/login/")
 def login():
     return render_template("admin/login.html")
 
-@admin.route("/logout")
+@admin.route("/logout/")
 def logout():
     return redirect(url_for("admin.login"))
+
+@admin.route("/pwd/")
+def pwd():
+    return render_template("admin/pwd.html")
+
+@admin.route("/tag/add/")
+def tag_add():
+    return render_template("admin/tag_add.html")
+
+@admin.route("/tag/list/")
+def tag_list():
+    return render_template("admin/tag_list.html")
+
+@admin.route("/movie/add/")
+def movie_add():
+    return render_template("admin/movie_add.html")
+
+@admin.route("/movie/list/")
+def movie_list():
+    return render_template("admin/movie_list.html")
+
+
+
+
+
